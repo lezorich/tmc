7eb63957b7f82c9c5eb868a030f65db05e8958bf
Lukas Zorich <lukas.zorich@gmail.com>
2020-02-07 00:50:42 -0300

    Add heroku to allowed hosts

--- tmc/tmc/settings.py
+++ tmc/tmc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "hn*s+k9&r2k^iwv&m2s6%c+quue8)(y1t&9bv!k)n!)bj=+_4_"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
